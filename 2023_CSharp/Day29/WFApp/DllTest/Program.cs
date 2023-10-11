b82d07824e3afb409829a8f6bd11a75f9733b146
DESKTOP-80M99DU\CHEN <bbtzza@gmail.com>
2023-10-12 01:34:16 +0800

    Update day29

---
+++ 2023_CSharp/Day29/WFApp/DllTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using CalculateLibrary;
+namespace DllTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =10; int b = 20; int c = 30;
+            //實體化
+            CalculateLibrary.Class1 CalculateLibrary = new CalculateLibrary.Class1();
+            
+            // 使用DLL中的方法
+            int result_Add = CalculateLibrary.Add(a, b);
+            int result_Subtract = CalculateLibrary.Subtract(a, b);
+            int result_Multiply = CalculateLibrary.Multiply(a, b);
+
+            Console.WriteLine($"Add: {result_Add}");
+            Console.WriteLine($"Subtract: {result_Subtract}");
+            Console.WriteLine($"Multiply: {result_Multiply}");
+        }
+    }
+}
